7cee42dce663861daeed0d6d836ff71077783321
Jared Beach <jaredbeachdesign@gmail.com>
2017-06-03 10:33:54 -0500

    Fix login with updated urls and cookie (#9)
    * login with updated urls and cookie
    
    * style fixes and readme updates
    
    * style fixes and readme updates
    
    * fix eslintrc tab/space

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "off"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "brace-style": [
+            "error",
+            "1tbs"
+        ],
+        "curly": [
+            "error",
+            "multi-line"
+        ]
+    }
+};
---
+++ test/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "mocha": true,
+        "amd": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "off"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "brace-style": [
+            "error",
+            "1tbs"
+        ],
+        "curly": [
+            "error",
+            "multi-line"
+        ]
+    }
+};
