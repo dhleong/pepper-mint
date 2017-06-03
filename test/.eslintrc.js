5acbe8a1eac438adf4e866227da50f4846ec4dc3
Jared Beach <jaredbeachdesign@gmail.com>
2017-06-03 17:49:07 -0500

    Remove duplicate eslint rules in test directory (#10)
    * login with updated urls and cookie
    
    * style fixes and readme updates
    
    * style fixes and readme updates
    
    * fix eslintrc tab/space
    
    * no need to duplicate rules in test eslint

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -3,30 +3,5 @@ module.exports = {
         "es6": true,
         "mocha": true,
         "amd": true
-    },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "off"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "brace-style": [
-            "error",
-            "1tbs"
-        ],
-        "curly": [
-            "error",
-            "multi-line"
-        ]
     }
 };
