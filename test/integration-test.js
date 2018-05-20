d69fe2364a9cfe98b5a9b1d9aaa02cb305808155
Scott Giminiani <scott.giminiani@providence.org>
2018-05-19 17:15:21 -0700

    Add semicolons to end of all statements

--- test/integration-test.js
+++ test/integration-test.js
@@ -14,67 +14,67 @@ if (config) {
     describe('handles editing transactions', function () {
       it('verifies fields have changed after editing', async function () {
         this.timeout(30000);
-        let mint = await PepperMint(config.username, config.password, config.ius_session, config.thx_guid)
-        await createTransaction(mint)
-        let originalTransaction = (await getTransactions(mint))[0]
+        let mint = await PepperMint(config.username, config.password, config.ius_session, config.thx_guid);
+        await createTransaction(mint);
+        let originalTransaction = (await getTransactions(mint))[0];
 
-        let transactionUpdates = getTransactionUpdates(originalTransaction)
-        await editTransactionWithUpdates(mint, transactionUpdates)
-        let updatedTransaction = (await getTransactions(mint))[0]
+        let transactionUpdates = getTransactionUpdates(originalTransaction);
+        await editTransactionWithUpdates(mint, transactionUpdates);
+        let updatedTransaction = (await getTransactions(mint))[0];
 
-        await deleteTransaction(mint, updatedTransaction)
+        await deleteTransaction(mint, updatedTransaction);
 
-        await doAssertionsWithUpdates(updatedTransaction, transactionUpdates)
-      })
+        await doAssertionsWithUpdates(updatedTransaction, transactionUpdates);
+      });
       it('verifies undefined fields when updating will not be cleared after editing', async function () {
         this.timeout(30000);
-        let mint = await PepperMint(config.username, config.password, config.ius_session, config.thx_guid)
-        await createTransaction(mint)
-        let originalTransaction = (await getTransactions(mint))[0]
+        let mint = await PepperMint(config.username, config.password, config.ius_session, config.thx_guid);
+        await createTransaction(mint);
+        let originalTransaction = (await getTransactions(mint))[0];
 
-        let transactionUpdates = getTransactionUpdates(originalTransaction)
+        let transactionUpdates = getTransactionUpdates(originalTransaction);
         setAllOptionalFieldsUndefined(transactionUpdates);
 
-        await editTransactionWithUpdates(mint, transactionUpdates)
-        let updatedTransaction = (await getTransactions(mint))[0]
+        await editTransactionWithUpdates(mint, transactionUpdates);
+        let updatedTransaction = (await getTransactions(mint))[0];
 
-        await deleteTransaction(mint, updatedTransaction)
+        await deleteTransaction(mint, updatedTransaction);
 
-        await doAssertions(updatedTransaction, originalTransaction)
-      })
+        await doAssertions(updatedTransaction, originalTransaction);
+      });
       it('verifies empty fields when updating will not be cleared after editing', async function () {
         this.timeout(30000);
-        let mint = await PepperMint(config.username, config.password, config.ius_session, config.thx_guid)
-        await createTransaction(mint)
-        let originalTransaction = (await getTransactions(mint))[0]
+        let mint = await PepperMint(config.username, config.password, config.ius_session, config.thx_guid);
+        await createTransaction(mint);
+        let originalTransaction = (await getTransactions(mint))[0];
 
-        let transactionUpdates = getTransactionUpdates(originalTransaction)
+        let transactionUpdates = getTransactionUpdates(originalTransaction);
         setAllFieldsEmpty(transactionUpdates);
 
-        await editTransactionWithUpdates(mint, transactionUpdates)
-        let updatedTransaction = (await getTransactions(mint))[0]
+        await editTransactionWithUpdates(mint, transactionUpdates);
+        let updatedTransaction = (await getTransactions(mint))[0];
 
-        await deleteTransaction(mint, updatedTransaction)
+        await deleteTransaction(mint, updatedTransaction);
 
-        await doAssertions(updatedTransaction, originalTransaction)
+        await doAssertions(updatedTransaction, originalTransaction);
       })
     });
   });
 }
 
 function doAssertions(updatedTransaction, originalTransaction) {
-  updatedTransaction.merchant.should.equal(originalTransaction.merchant)
-  updatedTransaction.category.should.equal(originalTransaction.category)
-  updatedTransaction.categoryId.should.equal(originalTransaction.categoryId)
-  updatedTransaction.date.should.equal(originalTransaction.date)
-  updatedTransaction.note.should.equal(originalTransaction.note)
+  updatedTransaction.merchant.should.equal(originalTransaction.merchant);
+  updatedTransaction.category.should.equal(originalTransaction.category);
+  updatedTransaction.categoryId.should.equal(originalTransaction.categoryId);
+  updatedTransaction.date.should.equal(originalTransaction.date);
+  updatedTransaction.note.should.equal(originalTransaction.note);
 }
 
 function doAssertionsWithUpdates(updatedTransaction, transactionUpdates) {
-  updatedTransaction.merchant.should.equal(transactionUpdates.merchant)
-  updatedTransaction.category.should.equal(transactionUpdates.category)
-  updatedTransaction.categoryId.should.equal(transactionUpdates.categoryId)
-  updatedTransaction.date.should.equal(formatDate(transactionUpdates.date))
+  updatedTransaction.merchant.should.equal(transactionUpdates.merchant);
+  updatedTransaction.category.should.equal(transactionUpdates.category);
+  updatedTransaction.categoryId.should.equal(transactionUpdates.categoryId);
+  updatedTransaction.date.should.equal(formatDate(transactionUpdates.date));
 }
 
 function createTransaction(mint) {
@@ -83,8 +83,8 @@ function createTransaction(mint) {
     date: "05/04/2010",
     merchant: "Test Merchant Name",
     note: "This is a test transaction"
-  }
-  return mint.createTransaction(createRequest)
+  };
+  return mint.createTransaction(createRequest);
 }
 
 function getTransactions(mint) {
@@ -94,12 +94,12 @@ function getTransactions(mint) {
     ],
     startDate: new Date(2010, 4),
     endDate: new Date(2010, 6)
-  }
-  return mint.getTransactions(getTransactionsRequest)
+  };
+  return mint.getTransactions(getTransactionsRequest);
 }
 
 function editTransactionWithUpdates(mint, updates) {
-  return mint.editTransaction(updates)
+  return mint.editTransaction(updates);
 }
 
 function getTransactionUpdates(originalTransaction) {
@@ -117,28 +117,28 @@ function deleteTransaction(mint, transactions) {
 }
 
 function setAllFieldsEmpty(transactionUpdates) {
-  transactionUpdates.merchant = ""
-  transactionUpdates.category = ""
-  transactionUpdates.categoryId = ""
-  transactionUpdates.date = ""
+  transactionUpdates.merchant = "";
+  transactionUpdates.category = "";
+  transactionUpdates.categoryId = "";
+  transactionUpdates.date = "";
 }
 
 function setAllOptionalFieldsUndefined(transactionUpdates) {
-  transactionUpdates.merchant = undefined
-  transactionUpdates.category = undefined
-  transactionUpdates.categoryId = undefined
+  transactionUpdates.merchant = undefined;
+  transactionUpdates.category = undefined;
+  transactionUpdates.categoryId = undefined;
   // Date is a required field
-  transactionUpdates.date = ""
+  transactionUpdates.date = "";
 }
 
 function formatDate(currentYearStyledDate) {
-  let date = new Date(currentYearStyledDate)
-  let year = date.getFullYear().toString().slice(2)
-  let month = padLeadingZero(date.getMonth() + 1)
-  let day = padLeadingZero(date.getDate())
+  let date = new Date(currentYearStyledDate);
+  let year = date.getFullYear().toString().slice(2);
+  let month = padLeadingZero(date.getMonth() + 1);
+  let day = padLeadingZero(date.getDate());
   return `${month}/${day}/${year}`
 }
 
 function padLeadingZero(number) {
-  return `0${number.toString()}`.slice(-2)
+  return `0${number.toString()}`.slice(-2);
 }
